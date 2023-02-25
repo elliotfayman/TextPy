c65abe947b7c620f2f9d65e9b2b969e4b4729e6b
Ryan-Kao <ryan.kao.930@my.csun.edu>
2023-02-24 17:30:14 -0800

    Create structure for root URL API endpoint

---
+++ Flask/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+# in_memory_datastore = { }
+
+@app.route('/')
+def homepage():
+    return render_template("/home.html")
+    # return {"messages":list(in_memory_datastore.values())}
+
+if __name__ == '__main__':
+    app.run(debug=True)
