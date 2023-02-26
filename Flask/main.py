a45ff5e2aac51d2baf48433485ab938a7091bede
Ryan-Kao <ryan.kao.930@my.csun.edu>
2023-02-25 18:44:22 -0800

    Create API documentation html page

--- Flask/main.py
+++ Flask/main.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-# in_memory_datastore = { }
-
 @app.route('/')
+@app.route('/home')
 def homepage():
     return render_template("/home.html")
-    # return {"messages":list(in_memory_datastore.values())}
 
 if __name__ == '__main__':
     app.run(debug=True)
