273b7f1d09aa48b7ad76aa6e42ab45ecc6b9a222
Ryan-Kao <ryan.kao.930@my.csun.edu>
2023-04-06 15:31:43 -0700

    Receive and Responding to SMS message

--- Flask/main.py
+++ Flask/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_restful import Resource, Api
 from flask import jsonify
+from twilio import twiml
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,9 +11,18 @@ api = Api(app)
 def homepage():
     return render_template("/home.html")
 
+@app.route('/sms', methods=['POST'])
+def sms():
+    number = request.form['From']
+    message_body = request.form['Body']
+
+    resp = twiml.Response()
+    resp.message('Hello {}, you said: {}'.format(number, message_body))
+    return str(resp)
 
 @app.route('/test')
 def test():
     return jsonify({'code': 200, 'message': 'Server contacted. Connection successful'})
+
 if __name__ == '__main__':
     app.run(debug=True)
